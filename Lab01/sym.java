986cd472ddb8787a83dd7c2b1248865b426fd1a1
Lazar Becic <lazarrr@elfak.rs>
2024-11-17 20:45:19 +0100

    Added lab 01

---
+++ Lab01/sym.java
@@ -0,0 +1,28 @@
+public class sym {
+
+    public final static int EOF = 0;               // Kraj fajla
+    public final static int MAIN = 1;              // Ključna reč 'main'
+    public final static int BLOCK = 2;             // Blok 'Block'
+    public final static int DEclarations = 3;      // Declarations
+    public final static int TYPE_INT = 4;          // Tip podataka 'int'
+    public final static int TYPE_FLOAT = 5;        // Tip podataka 'float'
+    public final static int TYPE_BOOL = 6;         // Tip podataka 'bool'
+    public final static int EXIT = 7;              // Ključna reč 'exit'
+    public final static int FOR = 8;               // Ključna reč 'for'
+    public final static int IN = 9;                // Ključna reč 'in'
+    public final static int APPLY = 10;            // Ključna reč 'apply'
+    public final static int ID = 11;               // Identifikator (ID)
+    public final static int CONST = 12;            // Konstanta
+    public final static int PLUS = 13;             // Operator '+'
+    public final static int MINUS = 14;            // Operator '-'
+    public final static int ASSIGN = 15;           // Operator ':='
+    public final static int SEMICOLON = 16;        // Separator ';'
+    public final static int SQUARELEFTPAR = 17;    // Levo uglasto zagrade '['
+    public final static int SQUARERIGHTPAR = 18;   // Desno uglasto zagrade ']'
+    public final static int COMMA = 19;            // Separator ','
+    public final static int DOT = 20;              // Operator '.'
+    public final static int EQUALS = 21;           // Operator '='
+    public final static int TRUE = 22;             // Ključna reč 'true'
+    public final static int FALSE = 23;            // Ključna reč 'false'
+
+}
